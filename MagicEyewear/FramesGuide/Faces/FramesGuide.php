5a3a7528a1bb6ceda1572050dc981f084c730eb8
yaraamrsalah <56272521+yaraamrsalah@users.noreply.github.com>
2021-05-08 23:27:49 +0200

    Add files via upload

---
+++ MagicEyewear/FramesGuide/Faces/FramesGuide.php
@@ -0,0 +1,92 @@
+<?php
+   $path = $_SERVER['DOCUMENT_ROOT'];
+   $path .= "/MagicEyewear/Menu/Menu.php";
+   include ($path);
+?>
+<!DOCTYPE html>
+<html>
+<head>
+	<title></title>
+</head>
+<body>
+
+<div class="faces">
+  <div class="container">
+    <h3> Choose Your Face Shape And We Will Tell You Which Glasses You Should Get</h3>
+      <div class="guide">
+      <!-- <div class="col-md-3"> -->
+        <div class="view view-ninth"><a href="Diamond.php">
+                  <img src="FacesPics/diamond-shape.png" class="img-responsive" alt="diamond-shaped"/>
+                  <div class="mask mask-1"> </div>
+                  <div class="mask mask-2"> </div>
+                    <div class="content">
+                      <h2>Diamond-Shaped</h2>
+                      <p>Click to Learn More</p>
+                    </div>
+                 </a> </div>
+               <!-- </div> -->
+        <!-- <div class="col-md-3"> -->
+          <div class="view view-ninth"><a href="Heart.php">
+                    <img src="FacesPics/Heart-Shape.png" class="img-responsive" alt="heart-shaped"/>
+                    <div class="mask mask-1"> </div>
+                    <div class="mask mask-2"> </div>
+                      <div class="content">
+                        <h2>Heart-Shaped</h2>
+                        <p>Click to Learn More</p>
+                      </div>
+                   </a> </div>
+                 <!-- </div> -->
+                 <!-- <div class="col-md-3"> -->
+                   <div class="view view-ninth"><a href="Oval.php">
+                             <img src="FacesPics/Oval-Shape.png" class="img-responsive" alt="oval-shaped"/>
+                             <div class="mask mask-1"> </div>
+                             <div class="mask mask-2"> </div>
+                               <div class="content">
+                                 <h2>Oval-Shaped</h2>
+                                 <p>Click to Learn More</p>
+                               </div>
+                            </a> </div>
+           <!-- </div> -->
+
+        <!-- <div class="col-md-4"> -->
+          <div class="view view-ninth"><a href="Round.php">
+                    <img src="FacesPics/Round-Shape.png" class="img-responsive" alt="round-shaped"/>
+                    <div class="mask mask-1"> </div>
+                    <div class="mask mask-2"> </div>
+                      <div class="content">
+                        <h2>Round</h2>
+                        <p>Click to Learn More</p>
+                      </div>
+                   </a> </div>
+                 <!-- </div> -->
+                 <!-- <div class="col-md-3"> -->
+                   <div class="view view-ninth"><a href="Square.php">
+                             <img src="FacesPics/Square-Shape.png" class="img-responsive" alt="square-shaped"/>
+                             <div class="mask mask-1"> </div>
+                             <div class="mask mask-2"> </div>
+                               <div class="content">
+                                 <h2>Square</h2>
+                                 <p>Click to Learn More</p>
+                               </div>
+                            </a> </div>
+                          <!-- </div> -->
+                          <!-- <div class="col-md-3"> -->
+                            <div class="view view-ninth"><a href="Triangle.php">
+                                      <img src="FacesPics/Triangle-Shape.png" class="img-responsive" alt="triangle-shaped"/>
+                                      <div class="mask mask-1"> </div>
+                                      <div class="mask mask-2"> </div>
+                                        <div class="content">
+                                          <h2>Triangular</h2>
+                                          <p>Click to Learn More</p>
+                                        </div>
+                                     </a> </div>
+                                   <!-- </div> -->
+      </div>
+
+    </div>
+   </div>
+
+
+</body>
+
+</html>
---
+++ MagicEyewear/FramesGuide/Faces/Square.php
@@ -0,0 +1,22 @@
+<?php
+   $path = $_SERVER['DOCUMENT_ROOT'];
+   $path .= "/MagicEyewear/Menu/Menu.php";
+   include ($path);
+?>
+<!DOCTYPE html>
+<html>
+<head>
+	<title></title>
+</head>
+<body>
+  <hr>
+  <br>
+<h3 class="m_2">So Your Face is Square-Shaped, Now What?</h3>
+<div class="polaroid">
+  <img src="FacesPics/Square-Shape.png" alt="square-shaped">
+  <div class="containerP">
+    <p>Square-Shaped Face</p>
+  </div>
+</div>
+</body>
+</html>
