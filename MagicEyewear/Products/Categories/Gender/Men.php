f4bbe2df8abd857cecc6bc4966b3c94fb59afd92
yaraamrsalah <56272521+yaraamrsalah@users.noreply.github.com>
2021-05-09 14:35:20 +0200

    Update Men.php

--- MagicEyewear/Products/Categories/Gender/Men.php
+++ MagicEyewear/Products/Categories/Gender/Men.php
@@ -1,1 +1,15 @@
 
+<?php
+   $path = $_SERVER['DOCUMENT_ROOT'];
+   $path .= "/MagicEyewear/Menu/Menu.php";
+   include ($path);
+?>
+<!DOCTYPE html>
+<html>
+<head>
+	<title></title>
+</head>
+<body>
+<h1>Boys</h1>
+</body>
+</html>
