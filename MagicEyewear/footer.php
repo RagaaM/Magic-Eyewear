063b3ba8cb3a91d282d9349cb11acb18c14bf591
RagaaM <50842401+RagaaM@users.noreply.github.com>
2021-05-09 12:36:52 +0200

    Create footer.php

---
+++ MagicEyewear/footer.php
@@ -0,0 +1,56 @@
+    <footer class="mainfooter" role="contentinfo">
+  <div class="footer-middle">
+  <div class="container">
+    <div class="row">
+      <div class="col-md-3 col-sm-6">
+        <!--Column1-->
+        <div class="footer-pad">
+          <h4> Shop</h4>
+          <ul class="list-unstyled">
+            
+            <li><a href="/MagicEyewear/Products/Gallery/Eyeglasses.php">Eyeglasses</a></li>
+            <li><a href="/MagicEyewear/Products/Gallery/Sunglasses.php">Sunglasses</a></li>
+            <li><a href="/MagicEyewear/Products/Gallery/Sports.php">Sports</a></li>
+							<li><a href="/MagicEyewear/Products/Gallery/Brands.php">Shop By Brand</a></li>
+          </ul>
+        </div>
+      </div>
+      <div class="col-md-3 col-sm-6">
+        <!--Column1-->
+        <div class="footer-pad">
+          <h4>Discover</h4>
+          <ul class="list-unstyled">
+          <li><a href="/MagicEyewear/Discover/AboutUs.php">About Us</a></li>
+							<li><a href="/MagicEyewear/Discover/FramesGuide/Guide.php">Face Shape Guide for Glasses</a></li>
+							<li><a href="#">Virtual Try-On</a></li>
+          </ul>
+        </div>
+      </div>
+      <div class="col-md-3 col-sm-6">
+        <!--Column1-->
+        <div class="footer-pad">
+          <h4>Location</h4>
+          <ul class="list-unstyled">
+          <li >1 Dmshek, St، Maadi, Cairo Governorate</li>
+            <li> <a href="#"></a> </li>
+          </ul>
+        </div>
+      </div>
+      <div class="col-md-3">
+        <h4>Follow Us</h4>
+            <ul class="social-network social-circle">
+             <li><a href="https://www.facebook.com/EyecareOpticsEgypt" class="icoFacebook" title="Facebook"><i class="fa fa-facebook"></i></a></li>
+             <li><a href="https://www.instagram.com/eyecareoptics.co/" class="icoInstagram" title="Instagram"><i class="fa fa-instagram"></i></a></li>
+            </ul>       
+    </div>
+    </div>
+  <div class="row">
+    <div class="col-md-12 copy">
+      
+    </div>
+  </div>
+
+
+  </div>
+  </div>
+</footer>
