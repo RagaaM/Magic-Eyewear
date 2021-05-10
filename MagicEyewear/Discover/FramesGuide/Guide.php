3a385216bea5d94bd971c7bbc312ff431e0ea742
yaraamrsalah <56272521+yaraamrsalah@users.noreply.github.com>
2021-05-10 21:24:36 +0200

    Update Guide.php

--- MagicEyewear/Discover/FramesGuide/Guide.php
+++ MagicEyewear/Discover/FramesGuide/Guide.php
@@ -9,6 +9,32 @@
 <link rel="stylesheet" href="/MagicEyewear/Layout/guide.css?v=<?php echo time(); ?>">
 </head>
 <body>
-<h1>eyeglasses</h1>
+      <h2> Choose Your Face Shape And We Will Tell You Which Glasses You Should Get</h2>
+      <div class="face-shapes">
+        <!-- First Row -->
+          <div class="face-overlay">
+            <div class=""
+            <img src="/MagicEyewear/Discover/FramesGuide/Faces/FacesPics/Diamond-Shape.png" alt="diamond shape"/>
+          </div>
+          <div class="face-overlay">
+            <img src="/MagicEyewear/Discover/FramesGuide/Faces/FacesPics/Heart-Shape.png" alt="heart shape"/>
+          </div>
+          <div class="face-overlay">
+            <img src="/MagicEyewear/Discover/FramesGuide/Faces/FacesPics/Oval-Shape.png" alt="oval shape"/>
+          </div>
+
+        <!-- Second Row -->
+        <div class="face-overlay">
+          <img src="/MagicEyewear/Discover/FramesGuide/Faces/FacesPics/Round-Shape.png" alt="round shape"/>
+        </div>
+        <div class="face-overlay">
+          <img src="/MagicEyewear/Discover/FramesGuide/Faces/FacesPics/Square-Shape.png" alt="square shape"/>
+        </div>
+        <div class="face-overlay">
+          <img src="/MagicEyewear/Discover/FramesGuide/Faces/FacesPics/Triangle-Shape.png" alt="triangular shape"/>
+        </div>
+
+      </div>
+
 </body>
 </html>
