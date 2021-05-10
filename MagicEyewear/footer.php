2ac10cec8b00768123b8dec804c8387a120c2476
RagaaM <50842401+RagaaM@users.noreply.github.com>
2021-05-10 02:41:17 +0200

    Update footer.php

--- MagicEyewear/footer.php
+++ MagicEyewear/footer.php
@@ -1,4 +1,15 @@
-    <footer class="mainfooter" role="contentinfo">
+<head>
+<link rel="stylesheet" href="/MagicEyewear/Layout/footer.css">
+<link href="https://maxcdn.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css" rel="stylesheet" >
+<!--
+<meta name="viewport" content="width=device-width, initial-scale=1">
+  <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css">
+  <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
+  <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.3/umd/popper.min.js"></script>
+  <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.1.3/js/bootstrap.min.js"></script>
+   <link href="https://maxcdn.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css" rel="stylesheet" -->
+</head>
+<footer class="mainfooter" role="contentinfo">
   <div class="footer-middle">
   <div class="container">
     <div class="row">
@@ -49,8 +60,6 @@
       
     </div>
   </div>
-
-
   </div>
   </div>
 </footer>
