17545a635ff9ed9800c514e2bc5be0cb738dd10d
RagaaM <50842401+RagaaM@users.noreply.github.com>
2021-05-10 17:17:39 +0200

    Update Brands.php

--- MagicEyewear/Products/Gallery/Brands.php
+++ MagicEyewear/Products/Gallery/Brands.php
@@ -1,15 +1,66 @@
-
 <?php
    $path = $_SERVER['DOCUMENT_ROOT'];
    $path .= "/MagicEyewear/Menu/Menu.php";
    include ($path);
 ?>
-<!DOCTYPE html>
 <html>
 <head>
-	<title></title>
+	<title>Shop By Brand </title>
+	<link rel="stylesheet" href="/MagicEyewear/Layout/shopbybrand.css">
 </head>
 <body>
-<h1>Boys</h1>
-</body>
+
+  
+<h1> Shop By Brand  </h1> 
+
+<div class="content">
+			<div class="categories">
+				<a href="" target="_blank" class="image-title">
+				<img src="/MagicEyewear/images/Ray-Ban.png" class="image-item"/>
+				
+			</div>	
+		<a href="#armani" target="_blank">
+			<div class="categories">
+				<img src="/MagicEyewear/images/armani.png" class="image-item"/>
+				
+			</div>
+		</a> 
+		
+		<a href="#tommyhilfiger"> 
+			<div class="categories">
+				<img src="/MagicEyewear/images/tommyhilfiger.jpg" class="image-item"/>
+				
+			</div>
+			<a href="#Ralph-Lauren">
+			<div class="categories">
+				<img src="/MagicEyewear/images/Ralph-Lauren.png" class="image-item"/>
+				
+			</div>
+		</a>
+			<a href="#PierreCardin">
+			<div class="categories">
+				<img src="/MagicEyewear/images/PierreCardin.png" class="image-item"/>
+				
+			</div>
+		</a>
+		<a href="#seven">
+			<div class="categories">
+				<img src="/MagicEyewear/images/seven.jpeg" class="image-item"/>
+				
+			</div>
+		</a>
+			<a href="#Polaroid">
+			<div class="categories">
+				<img src="/MagicEyewear/images/Polaroid.png" class="image-item"/>
+				
+			</div>
+		</a>
+		<a href="#boss">
+			<div class="categories">
+				<img src="/MagicEyewear/images/boss.jfif" class="image-item"/>
+				
+			</div>
+		</a>
+		</div>
+		</body>
 </html>
