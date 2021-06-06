6f6f1d11c8d2b84a943da77a788dff31bbe26273
yaraamrsalah <yaraamrsalahh@gmail.com>
2021-06-06 19:36:01 +0200

    Some Updates
    - Converted database connection to PDO
    
    - Delete UserClass.php

--- MagicEyewear/Database/DatabaseConnection.php
+++ MagicEyewear/Database/DatabaseConnection.php
@@ -1,28 +1,20 @@
 <?php
 class DatabaseConnection{
 
-    private $host = 'localhost';
-    private $username = 'root';
-    private $password = '';
-    private $database = 'magiceyewear';
-
-    protected $connection;
-
-    public function __construct(){
-
-        if (!isset($this->connection)) {
-
-            $this->connection = new mysqli($this->host, $this->username, $this->password, $this->database);
-
-            if (!$this->connection) {
-                echo 'Cannot connect to database';
-                exit;
-            }
-        }
-
-        return $this->connection;
+        private $host="localhost";
+        private $user="root";
+        private $pass="";
+        private $database="magic_eyewear";
+    
+      protected function connect(){
+        $conn = 'mysql:host='.$this->host.';database='.$this->database;
+        $pdo = new PDO($conn,$this->user,$this->pass);
+        $pdo ->setAttribute(PDO::ATTR_DEFAULT_FETCH_MODE, PDO::FETCH_ASSOC);
+        return $pdo;
+    
+      }
     }
-}
+
 
 
 
