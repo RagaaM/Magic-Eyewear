2fcec4a6adc21a0a24f74513adc40190022013ef
yaraamrsalah <yaraamrsalahh@gmail.com>
2021-06-10 04:39:34 +0200

    Menu Template

--- MagicEyewear/App/View/Includes/TopNav.php
+++ MagicEyewear/App/View/Includes/TopNav.php
@@ -1,1 +1,90 @@
-<!-- MENU -->
+<html>
+<head>
+<meta name="viewport" content="width=device-width, initial-scale=1">
+    <meta name="robots" content="all,follow">
+     <!-- Bootstrap CSS-->
+    <link rel="stylesheet" href="/Magic-Eyewear/MagicEyewear/Public/vendor/bootstrap/css/bootstrap.min.css">
+    <!-- Font Awesome CSS-->
+    <link rel="stylesheet" href="/Magic-Eyewear/MagicEyewear/Public/vendor/font-awesome/css/font-awesome.min.css">
+    
+    <!-- Google fonts - Roboto-->
+    <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Roboto:300,400,700">
+   <!-- theme stylesheet-->
+    <link rel="stylesheet" href="/Magic-Eyewear/MagicEyewear/Public/CSS/TopNav.css" id="theme-stylesheet">
+</head>
+<body>
+<div id="all">
+    <header class="nav-holder make-sticky">
+        <div id="navbar" class="navbar navbar-expand-lg" role="navigation" >
+            <div class="container">
+                <a href="/MagicEyewear/Public/Index.php" class="navbar-brand home">
+                <img src="/MagicEyewear/Public/Images/eyecare-logo.png" alt="EyeCare Optics logo" class="d-none d-md-inline-block"></a>
+                <button type="button" data-toggle="collapse" data-target="#navigation" class="navbar-toggler btn-template-outlined">
+                    <span class="sr-only">Toggle navigation</span> <i class="fa fa-align-justify"></i></button>
+                <div id="navigation" class="navbar-collapse collapse">
+                <ul class="nav navbar-nav ml-auto">
+                <li class="nav-item dropdown menu-large">
+                    <a href="#" data-toggle="dropdown" class="dropdown-toggle">Optical Glasses<b class="caret"></b></a>
+                    <ul class="dropdown-menu megamenu">
+
+                    </ul>
+                </li>
+                <li class="nav-item dropdown menu-large">
+                    <a href="#" data-toggle="dropdown" class="dropdown-toggle">Sunglasses<b class="caret"></b></a>
+                    <ul class="dropdown-menu megamenu">
+                        
+                    </ul>
+                </li>
+                <li class="nav-item dropdown menu-large">
+                    <a href="#" data-toggle="dropdown" class="dropdown-toggle">Sports<b class="caret"></b></a>
+                    <ul class="dropdown-menu megamenu">
+                        
+                    </ul>
+                </li>
+                <li><a href="">About Us</a></li>
+                <li><a href="">Frames Guide</a></li>
+                <li><a href="/Magic-Eyewear/MagicEyewear/App/View/User/LoginRegisterForm.php">Login</a></li>
+
+                <!-- SHOW WHEN CLIENT IS LOGGED IN -->
+                    <!-- <li><a href="">Profile</a></li>
+                    <li><a href="">Cart</a></li>
+                    <li><a href="">Logout</a></li> -->
+
+
+                </ul>
+                </div>
+            </div>            
+
+
+
+
+        </div> <!-- End of navbar div -->
+
+
+
+
+
+
+
+
+    </header>
+
+
+
+
+
+
+
+
+
+
+
+</div> <!-- End of first div -->
+
+ <!-- Javascript files for toggle menu-->
+    <script src="/Magic-Eyewear//MagicEyewear/Public/vendor/jquery/jquery.min.js"></script>
+    <script src="/Magic-Eyewear//MagicEyewear/Public/vendor/bootstrap/js/bootstrap.min.js"></script>
+    
+
+</body>
+</html>
